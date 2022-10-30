0068967d442e34acc01f03c0c993b22fac90a71f
Grayson Harvey <graycodes.nu@gmail.com>
2022-10-29 20:51:36 -0500

    tried linking components to the navbar using routes, but have not been able to figure it out.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,46 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import AboutMe from "./AboutMe";
+import Portfolio from "./Portfolio";
+import Contact from "./Contact";
+import Resume from "./Resume";
 
-
-export default function Header () {
-
+export default function Header() {
   return (
     <>
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          {/* <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton> */}
-          <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
-            Grayson Harvey
-          </Typography>
-          <Button color="inherit">About Me</Button>
-          <Button color="inherit">Portfolio</Button>
-          <Button color="inherit">Contact</Button>
-          <Button color="inherit">Resume</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-
-
+      <Router>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+                Grayson Harvey
+              </Typography>
+              <Link to={AboutMe}>
+                <Button color="inherit">About Me</Button>
+              </Link>
+              <Link to={Portfolio}>
+                <Button color="inherit">Portfolio</Button>
+              </Link>
+              <Link to={Contact}>
+                <Button color="inherit">Contact</Button>
+              </Link>
+              <Link to={Resume}>
+                <Button color="inherit">Resume</Button>
+              </Link>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <Routes>
+          <Route exact path="/AboutMe" element={<AboutMe />}></Route>
+          <Route exact path="/Portfolio" element={<Portfolio />}></Route>
+          <Route exact path="/Contact" element={<Contact />}></Route>
+          <Route exact path="/Resume" element={<Resume />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -12,156 +12,158 @@ export default function Portfolio () {
   return (
       <>
       <h2>Portfolio</h2>
-{/* *********** goodWeeds *********** */}
       <div>
-      <Card sx={{ maxWidth: 345, maxHeight: 300 }}>
-      <CardActionArea href="https://goodweeds.herokuapp.com/">
-        <CardMedia
-          component="img"
-          height="200"
-          image={require('../assets/goodWeeds.png')}
-          alt="goodWeeds"
-        />
-        </CardActionArea>
+{/* *********** goodWeeds *********** */}
+        <div>
+        <Card sx={{ maxWidth: 345, maxHeight: 300 }}>
+        <CardActionArea href="https://goodweeds.herokuapp.com/">
+          <CardMedia
+            component="img"
+            height="200"
+            image={require('../assets/goodWeeds.png')}
+            alt="goodWeeds"
+          />
+          </CardActionArea>
 
-      <CardActions>
-        <Typography variant='h5'>
-          goodWeeds
-        </Typography>
-        <IconButton href="https://goodweeds.herokuapp.com/">
-          <InsertLinkTwoToneIcon />
-        </IconButton>
-        <IconButton href="https://github.com/The-Brain-Cell-Collective/goodWeeds" color='primary'>
-        <GitHubIcon />
-        </IconButton>
-      </CardActions>
-      </Card>
-      </div>
+        <CardActions>
+          <Typography variant='h5'>
+            goodWeeds
+          </Typography>
+          <IconButton href="https://goodweeds.herokuapp.com/">
+            <InsertLinkTwoToneIcon />
+          </IconButton>
+          <IconButton href="https://github.com/The-Brain-Cell-Collective/goodWeeds" color='primary'>
+          <GitHubIcon />
+          </IconButton>
+        </CardActions>
+        </Card>
+        </div>
 
 {/* *********** INtertain *********** */}
-      <div>
-      <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea href="https://nw-bootcamp-team1.github.io/Night-In/home-page/index">
-        <CardMedia
-          component="img"
-          height="200"
-          image={require('../assets/INtertain.png')}
-          alt="INtertain"
-        />
-        </CardActionArea>
-      <CardActions>
-      <Typography variant='h5'>
-          INtertain
-        </Typography>
-        <IconButton href="https://nw-bootcamp-team1.github.io/Night-In/home-page/index">
-          <InsertLinkTwoToneIcon />
-        </IconButton>
-        <IconButton href="https://github.com/NW-Bootcamp-Team1/Night-In" color='primary'>
-        <GitHubIcon />
-        </IconButton>
-      </CardActions>
-      </Card>
-      </div>
-
-{/* *********** Workday Scheduler *********** */}
-    <div>
-      <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea href="https://graycodesnu.github.io/work-day-scheduler/">
-        <CardMedia
-          component="img"
-          height="200"
-          image={require('../assets/workday-scheduler.png')}
-          alt="Workday Scheduler"
-        />
-        </CardActionArea>
+        <div>
+        <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea href="https://nw-bootcamp-team1.github.io/Night-In/home-page/index">
+          <CardMedia
+            component="img"
+            height="200"
+            image={require('../assets/INtertain.png')}
+            alt="INtertain"
+          />
+          </CardActionArea>
         <CardActions>
         <Typography variant='h5'>
-          Workday Scheduler
-        </Typography>
-        <IconButton href="https://graycodesnu.github.io/work-day-scheduler/">
-          <InsertLinkTwoToneIcon />
-        </IconButton>
-        <IconButton href="https://github.com/graycodesnu/work-day-scheduler" color='primary'>
-        <GitHubIcon />
-        </IconButton>
-      </CardActions>
-      </Card>
+            INtertain
+          </Typography>
+          <IconButton href="https://nw-bootcamp-team1.github.io/Night-In/home-page/index">
+            <InsertLinkTwoToneIcon />
+          </IconButton>
+          <IconButton href="https://github.com/NW-Bootcamp-Team1/Night-In" color='primary'>
+          <GitHubIcon />
+          </IconButton>
+        </CardActions>
+        </Card>
+        </div>
+
+{/* *********** Workday Scheduler *********** */}
+      <div>
+        <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea href="https://graycodesnu.github.io/work-day-scheduler/">
+          <CardMedia
+            component="img"
+            height="200"
+            image={require('../assets/workday-scheduler.png')}
+            alt="Workday Scheduler"
+          />
+          </CardActionArea>
+          <CardActions>
+          <Typography variant='h5'>
+            Workday Scheduler
+          </Typography>
+          <IconButton href="https://graycodesnu.github.io/work-day-scheduler/">
+            <InsertLinkTwoToneIcon />
+          </IconButton>
+          <IconButton href="https://github.com/graycodesnu/work-day-scheduler" color='primary'>
+          <GitHubIcon />
+          </IconButton>
+        </CardActions>
+        </Card>
       </div>
 
 {/* *********** Weather Dashboard *********** */}
-    <div>
-      <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea href="https://graycodesnu.github.io/weather-dashboard-week06/">
-        <CardMedia
-          component="img"
-          height="200"
-          image={require('../assets/weather-dash.png')}
-          alt="Weather Dashboard"
-        />
-        </CardActionArea>
-      <CardActions>
-        <Typography variant='h5'>
-          Weather Dashboard
-        </Typography>
-        <IconButton href="https://graycodesnu.github.io/weather-dashboard-week06/">
-          <InsertLinkTwoToneIcon />
-        </IconButton>
-        <IconButton href="https://github.com/graycodesnu/weather-dashboard-week06" color='primary'>
-        <GitHubIcon />
-        </IconButton>
-      </CardActions>
-      </Card>
+      <div>
+        <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea href="https://graycodesnu.github.io/weather-dashboard-week06/">
+          <CardMedia
+            component="img"
+            height="200"
+            image={require('../assets/weather-dash.png')}
+            alt="Weather Dashboard"
+          />
+          </CardActionArea>
+        <CardActions>
+          <Typography variant='h5'>
+            Weather Dashboard
+          </Typography>
+          <IconButton href="https://graycodesnu.github.io/weather-dashboard-week06/">
+            <InsertLinkTwoToneIcon />
+          </IconButton>
+          <IconButton href="https://github.com/graycodesnu/weather-dashboard-week06" color='primary'>
+          <GitHubIcon />
+          </IconButton>
+        </CardActions>
+        </Card>
       </div>
 
 {/* *********** Password Generator *********** */}
-    <div>
-      <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea href="https://graycodesnu.github.io/password_generator/">
-        <CardMedia
-          component="img"
-          height="200"
-          image={require('../assets/password-gen.png')}
-          alt="Password Generator"
-        />
-        </CardActionArea>
-      <CardActions>
-      <Typography variant='h5'>
-          Password Generator
-        </Typography>
-        <IconButton href="https://graycodesnu.github.io/password_generator/">
-          <InsertLinkTwoToneIcon />
-        </IconButton>
-        <IconButton href="https://github.com/graycodesnu/password_generator" color='primary'>
-        <GitHubIcon />
-        </IconButton>
-      </CardActions>
-      </Card>
+      <div>
+        <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea href="https://graycodesnu.github.io/password_generator/">
+          <CardMedia
+            component="img"
+            height="200"
+            image={require('../assets/password-gen.png')}
+            alt="Password Generator"
+          />
+          </CardActionArea>
+        <CardActions>
+        <Typography variant='h5'>
+            Password Generator
+          </Typography>
+          <IconButton href="https://graycodesnu.github.io/password_generator/">
+            <InsertLinkTwoToneIcon />
+          </IconButton>
+          <IconButton href="https://github.com/graycodesnu/password_generator" color='primary'>
+          <GitHubIcon />
+          </IconButton>
+        </CardActions>
+        </Card>
       </div>
 
 {/* *********** Express Notes *********** */}
-  <div>
-      <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea href="https://heroku-express-notes.herokuapp.com/">
-        <CardMedia
-          component="img"
-          height="200"
-          image={require('../assets/expressnotes.png')}
-          alt="Express Notes"
-        />
-        </CardActionArea>
-      <CardActions>
-      <Typography variant='h5'>
-          Express Notes
-        </Typography>
-        <IconButton href="https://heroku-express-notes.herokuapp.com/">
-          <InsertLinkTwoToneIcon />
-        </IconButton>
-        <IconButton href="https://github.com/graycodesnu/express-notes" color='primary'>
-        <GitHubIcon />
-        </IconButton>
-      </CardActions>
-      </Card>
+      <div>
+          <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea href="https://heroku-express-notes.herokuapp.com/">
+            <CardMedia
+              component="img"
+              height="200"
+              image={require('../assets/expressnotes.png')}
+              alt="Express Notes"
+            />
+            </CardActionArea>
+          <CardActions>
+          <Typography variant='h5'>
+              Express Notes
+            </Typography>
+            <IconButton href="https://heroku-express-notes.herokuapp.com/">
+              <InsertLinkTwoToneIcon />
+            </IconButton>
+            <IconButton href="https://github.com/graycodesnu/express-notes" color='primary'>
+            <GitHubIcon />
+            </IconButton>
+          </CardActions>
+          </Card>
       </div>
+    </div>
     </>
   );
 }
