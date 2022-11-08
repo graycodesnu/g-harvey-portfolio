2ef3db9296ce8e78dd840f24c989a5ff098f06f0
Grayson Harvey <graycodes.nu@gmail.com>
2022-11-07 21:51:32 -0600

    deployed!

--- src/data/resumeData.js
+++ src/data/resumeData.js
@@ -1,6 +1,6 @@
 const languages = ["JavaScript", "CSS", "HTML"]
-const frontend = ["ReactJS", "Material UI", "JQuery", "Bootstrap"]
-const backend = ["MySQL2", "ExpressJS", "NodeJS", "MongoDB", "Mongoose"]
+const frontend = ["React", "Material UI", "jQuery", "Bootstrap"]
+const backend = ["MySQL", "Express.js", "Node.js", "MongoDB", "Mongoose", "GraphQL"]
 
 export const proficiencies =
   { id: 1, languages, frontend, backend }
